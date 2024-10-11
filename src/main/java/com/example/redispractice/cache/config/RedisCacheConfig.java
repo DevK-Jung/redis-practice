bc0f41474db5ada56b2b04dcd721f44c262106f6
JungHyeonKim <dev.kjung@gmail.com>
2024-10-11 20:31:16 +0900

    [Feat]: Redis 의존성, 설정 추가

---
+++ src/main/java/com/example/redispractice/cache/config/RedisCacheConfig.java
@@ -0,0 +1,43 @@
+package com.example.redispractice.cache.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching // spring boot 캐싱 설정 활성화
+public class RedisCacheConfig {
+
+    @Bean
+    public CacheManager boardCacheManage(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration
+                .defaultCacheConfig()
+                .serializeKeysWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                                new StringRedisSerializer()
+                        )
+                ) // Redis에 Key 저장 시 String 으로 직렬화
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                                new Jackson2JsonRedisSerializer<Object>(Object.class)
+                        )
+                ) // Redis에 Value 저장 시 JSON 으로 직렬화
+                .entryTtl(Duration.ofMinutes(1L)); // 만료기간(TTL) 설정
+
+        return RedisCacheManager
+                .RedisCacheManagerBuilder
+                .fromConnectionFactory(redisConnectionFactory)
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
+    }
+
+}
